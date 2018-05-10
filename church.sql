168f4e8a02c0668a25ff548a1f481a41cd3fb858
ritaglo <ritahglory44@gmail.com>
2018-05-10 12:56:12 +0300

    update commit

--- church.sql
+++ church.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2018 at 11:13 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.0.21
+-- Generation Time: May 10, 2018 at 11:53 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -66,6 +66,18 @@ CREATE TABLE `event_types` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `parish`
 --
 
@@ -106,6 +118,32 @@ CREATE TABLE `subcounty` (
   `county_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'rita', 'echessa@gmail.com', '$2y$10$HuDDORydc4h.S97D0knmMeUvj7CXgj3Gep2DHuZ.fNZPNP79XJqse', '17S9tcYBgsdwU2lcAJdPuw6tsjpHujdOo9O6yZznhcgNLApLP3XBD9zaf5uf', '2018-05-05 06:56:21', '2018-05-05 06:56:21'),
+(2, 'Gloria', 'ritagloria@gmail.com', '$2y$10$0g82kgBVUiGCMubsH55l5u62IV57MSvla92GbI4uGl9gzisoWVzei', 'mG4sEbkYWG', '2018-05-10 04:51:35', '2018-05-10 04:51:35'),
+(3, 'evans', 'evans@gmail.com', '$2y$10$3D9d5k5QO04DoVC9InvUDOFLE6DskL1RylmSnF5hh6yoZHj9j9szi', 'HaZoE76fPO08liMWQtEX7roz97JNtsd38Utg0Dhyl6QLTyjVGTIp9y9tQ0qZ', '2018-05-10 06:40:31', '2018-05-10 06:40:31'),
+(4, 'joel', 'joelevans14@gmail.com', '$2y$10$tK3PktMcnVAx.LNdr1gBkucEo8JPTuk6lO6IrmmEyNy2G/wfPFj0S', '0CqDo6Q8dV8Dw0x1AfIsAAFnRHtYqn1lurCZDRnwJO11xMvgYwNNo17Ep6Rn', '2018-05-10 06:42:27', '2018-05-10 06:42:27');
+
 --
 -- Indexes for dumped tables
 --
@@ -131,6 +169,12 @@ ALTER TABLE `event_types`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `parish`
 --
 ALTER TABLE `parish`
@@ -153,6 +197,12 @@ ALTER TABLE `subcounty`
   ADD KEY `county_id` (`county_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -161,31 +211,49 @@ ALTER TABLE `subcounty`
 --
 ALTER TABLE `counties`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `diocese`
 --
 ALTER TABLE `diocese`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `event_types`
 --
 ALTER TABLE `event_types`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `parish`
 --
 ALTER TABLE `parish`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `pictures`
 --
 ALTER TABLE `pictures`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `subcounty`
 --
 ALTER TABLE `subcounty`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- Constraints for dumped tables
 --
---
+++ trial/app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    //
+    function index()
+    {
+        return view('login');
+        
+    }
+   // function login(){
+           // return();
+    //}
+}
